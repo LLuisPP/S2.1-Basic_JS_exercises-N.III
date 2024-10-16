bcd28dbd0317ee8cddbd035c3038059e058e6451
Borealuis <borealuis@MacBook-Pro-de-Borealuis.local>
2024-10-16 11:12:13 +0200

    Exercise 1 - arrow function

---
+++ js_basics.js
@@ -0,0 +1,17 @@
+
+// Exercici 1
+// Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma.
+// Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
+
+let a = 6;
+let b = 2;
+let function1;
+
+console.log("Variable values are a = ", a, " and b = ", b);
+
+function1 = (a, b) => a + b;
+
+
+console.log("Result of arrow function = ", function1(a, b));
+
+
